86b39eca08f1ab8bed7ad62bcc62318da7f1b6cc
GJunF <43266684+GJunF@users.noreply.github.com>
2019-06-21 13:18:34 +0800

    Add files via upload

---
+++ NoticeCheckerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Utils
+{
+    public class NoticeCheckerTest
+    {
+
+        public void Test1()
+        {
+            NoticeChecker<string> nc = new NoticeChecker<string>((str, invokeCount) =>
+            {
+                _Log.DebugFormat("{0}-{1}.", str, invokeCount);
+                return false;
+            }, 5, 5000, 2000);
+            nc.Notice("test");
+        }
+        public void Test2()
+        {
+            NoticeChecker<ModifyRemarkRequest> nc = new NoticeChecker<ModifyRemarkRequest>(ModifyRemark, 5, 1000, 5000);
+            nc.Notice(new ModifyRemarkRequest() { WechatFriendId = item.WechatFriendId, Remark = mark });
+        }
+        /// <summary>
+        /// /api/ThirdParty/modifyRemark 修改好友备注
+        /// </summary>
+        /// <param name="request">修改备注请求</param>
+        /// <returns>是否修改成功</returns>
+        public bool ModifyRemark(ModifyRemarkRequest request, int invokeCount)
+        {
+            BaseResponse response = ModifyRemark(request.WechatFriendId, request.Remark);
+            return response.Result == 0;
+        }
+
+    }
+}
