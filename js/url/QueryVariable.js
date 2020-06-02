48a26566750bb4a55cd6b381198e8358c1399fd6
GJunF <43266684+GJunF@users.noreply.github.com>
2020-06-02 11:35:50 +0800

    Create QueryVariable.js

---
+++ js/url/QueryVariable.js
@@ -0,0 +1,11 @@
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1),
+        vars = query.split("&"),
+        pair;
+    for (var i = 0; i < vars.length; i++) {
+        pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return unescape(pair[1]);
+        }
+    }
+}
